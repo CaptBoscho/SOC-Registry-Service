67ceb97b90f332e94ca376e8238a752075e6916f
Derek Argueta <darguetap@gmail.com>
2016-04-08 16:45:18 -0600

    file storage

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
-const path = require('path');     //used for file path
+const path = require('path');
+var multer = require('multer');
 const app = module.exports = express();
 
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(multer({dest:'./uploads/'}).single('jar'));
+ 
+app.post('/upload', function(req, res) {
+  console.log(req.file);
+  res.status(200).send('ayyyeee lmao');
+});
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const listen = process.env.NODE_IP || '0.0.0.0';
